47ab17fbf64056df2619a5c3685784cac21ddea9
Alan Pfeiffer <alan.fm.pfeiffer@gmail.com>
2014-08-14 00:33:14 -0400

    Adding dbparser things for basics page.

--- ConstructedLanguageOrganizerTool/ConstructedLanguageOrganizerTool/BasicsPage.xaml.cs
+++ ConstructedLanguageOrganizerTool/ConstructedLanguageOrganizerTool/BasicsPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace ConstructedLanguageOrganizerTool
 {
@@ -26,6 +27,18 @@ namespace ConstructedLanguageOrganizerTool
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            DatabaseParser dp = new DatabaseParser();
+            string fileDir = dp.GetFileLocation();
+            conlangsListBox.Items.Clear();
+
+            foreach (string file in Directory.EnumerateFiles(fileDir, "*.db"))
+            {
+                string filedb = file.Replace(fileDir, "");
+                filedb = filedb.Replace(".db", "");
+                conlangsListBox.Items.Add(filedb);
+            }
+
+
         }
 
         private void IPAValues_TextChanged(object sender, TextChangedEventArgs e)
@@ -54,28 +67,34 @@ namespace ConstructedLanguageOrganizerTool
 
 
         }
-        string conlange;
-        string IPASounds;
-        string conletters;
-        string basicwordform;
-
-        public BasicsPage()
-        {
-            conlange = "";
-            IPASounds = "";
-            conletters = "";
-            basicwordform = "";
-        }
+   
 
         private void conlangAddButton_Click(object sender, RoutedEventArgs e)
         {
             DatabaseParser dbp = new DatabaseParser();
-
-            
-            if (dbp.CreateDB(conlangNameValue.Text) == true)
-                MessageBox.Show("db already exists");
+            dbp.CreateDB(conlangNameValue.Text); //Create, if doesn't exist, and Open db.
+            int val;
+            if (int.TryParse(gendersValue.Text, out val))
+                dbp.AddOrUpdateDB(conlangNameValue.Text, IPAValues.Text, conlangLettersValue.Text, basicWordFormValue.Text, gendersValue.Text);
             else
-                MessageBox.Show("Db doesn't exist. Created " + conlangNameValue.Text);
+                MessageBox.Show("Error: Number of genders needs to be a number!");
+
+        }
+
+        private void conlangsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (conlangsListBox.SelectedItem != null)
+            {
+                DatabaseParser dbp = new DatabaseParser();
+                string[] dbEntry = dbp.ReadDB("Basics", conlangsListBox.SelectedItem.ToString());
+
+                conlangNameValue.Text = dbEntry[0];
+                IPAValues.Text = dbEntry[1];
+                conlangLettersValue.Text = dbEntry[2];
+                basicWordFormValue.Text = dbEntry[3];
+                gendersValue.Text = dbEntry[4];
+            }
+
 
 
         }
--- ConstructedLanguageOrganizerTool/ConstructedLanguageOrganizerTool/DatabaseParser.cs
+++ ConstructedLanguageOrganizerTool/ConstructedLanguageOrganizerTool/DatabaseParser.cs
@@ -9,7 +9,7 @@ namespace ConstructedLanguageOrganizerTool
 {
     class DatabaseParser
     {
-
+        string conlanguageName;
 
         public string GetFileLocation()
         {
@@ -19,52 +19,196 @@ namespace ConstructedLanguageOrganizerTool
             return fullpath;
         }
 
-
-        public bool CreateDB(string conlangName)
+        public string DBstring(string conlangName)
         {
             var dbFile = GetFileLocation();
-            dbFile = dbFile + conlangName + ".db";
+            conlanguageName = conlangName;
+            dbFile = dbFile + conlanguageName + ".db";
 
             var connString = string.Format(@"Data Source={0}; Pooling=false; FailIfMissing=false;", dbFile);
+
+            return connString;
+        }
+
+
+        public void CreateDB(string conlangName)
+        {
+            var connString = DBstring(conlangName);
+            
             SQLiteConnection dbConn = new SQLiteConnection(connString);
 
             dbConn.Open();
             SQLiteCommand cmd = dbConn.CreateCommand();
 
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS Basics (conlang string primary key, ipa string, letters string, basicForm string, genders string);";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS Lexicon (conword string primary key, localWord string, pronunciation string, pos string, gender string, declension string);";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS Grammar (grammarName string primary key, grammarType string, gramarPoint string);";
+            cmd.ExecuteNonQuery();
 
-            if (File.Exists(dbFile))
-            {
-                cmd.CommandText = @"CREATE TABLE IF NOT EXISTS Basics (conlang string primary key, ipa string, letters string, basicForm string);";
-                cmd.ExecuteNonQuery();
-                return true;
-            }
-            else
-            {
-                
-                return false;
-            }
+
+            dbConn.Close();
 
 
         }
 
         // Basics DB Add
-        public void AddOrUpdateDB(string conlangName, string ipaValue, string conletters, string basicWordForm, int genders)
+        public void AddOrUpdateDB(string conlangName, string ipaValue, string conletters, string basicWordForm, string genders)
         {
-            
+            var connString = DBstring(conlangName);
+
+            SQLiteConnection dbConn = new SQLiteConnection(connString);
+            dbConn.Open();
+            SQLiteCommand cmd = dbConn.CreateCommand();
+
+
+            cmd.CommandText = @"INSERT INTO Basics (conlang, ipa, letters, basicForm, genders) VALUES(@conlang, @ipa, @letters, @basicForm, @genders)";
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@conlang",
+                Value = conlangName
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@ipa",
+                Value = ipaValue
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@letters",
+                Value = conletters
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@basicForm",
+                Value = basicWordForm
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@genders",
+                Value = genders
+            });
+            cmd.ExecuteNonQuery();
+
+            dbConn.Close();
+
         }
 
         // Word DB Add
         public void AddOrUpdateDB(string conWord, string localWord, string pronunciation, string pos, int gender, string declension)
         {
+            var connString = DBstring(conlanguageName);
+            
+            SQLiteConnection dbConn = new SQLiteConnection(connString);
+            dbConn.Open();
+            SQLiteCommand cmd = dbConn.CreateCommand();
 
+
+            cmd.CommandText = @"INSERT INTO Lexicon (conlang, ipa, letters, basicForm, genders) VALUES(@conword, @localWord, @pronunciation, @pos, @gender, @declension)";
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@conword",
+                Value = conWord
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@localWord",
+                Value = localWord
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@pronunciation",
+                Value = pronunciation
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@pos",
+                Value = pos
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@gender",
+                Value = gender
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@declension",
+                Value = declension
+            });
+            cmd.ExecuteNonQuery();
+
+            dbConn.Close();
         }
 
         // Grammar DB Add
         public void AddOrUpdateDB(string grammarName, string grammarType, string grammarPoint)
         {
+            var connString = DBstring(conlanguageName);
+
+            SQLiteConnection dbConn = new SQLiteConnection(connString);
+            dbConn.Open();
+            SQLiteCommand cmd = dbConn.CreateCommand();
+
+
+            cmd.CommandText = @"INSERT INTO Grammar (grammarName, grammarType, grammarPoint) VALUES(@grammarName, @grammarType, @grammarPoint)";
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@grammarName",
+                Value = grammarName
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@grammarType",
+                Value = grammarType
+            });
+
+            cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
+            {
+                ParameterName = "@grammarPoint",
+                Value = grammarPoint
+            });
+
+            cmd.ExecuteNonQuery();
+
+            dbConn.Close();
 
         }
 
+        public string[] ReadDB(string page, string conlanguageName)
+        {
+            var connString = DBstring(conlanguageName);
+
+            SQLiteConnection dbConn = new SQLiteConnection(connString);
+            dbConn.Open();
+            SQLiteCommand cmd = dbConn.CreateCommand();
+
+            cmd.CommandText = @"select * from Basics";
+
+            System.Data.SQLite.SQLiteDataReader reader = cmd.ExecuteReader();
+
+            string[] dbValues = new string[reader.FieldCount];
+            while (reader.Read())
+                reader.GetValues(dbValues);
+
+            dbConn.Close();
+
+            return dbValues;
+
+        }
 
 
         public static void Test2(string connString)
@@ -77,27 +221,7 @@ namespace ConstructedLanguageOrganizerTool
                 dbConn.Open();
                 using (System.Data.SQLite.SQLiteCommand cmd = dbConn.CreateCommand())
                 {
-                    //create table
-                    cmd.CommandText = @"CREATE TABLE IF NOT EXISTS T1 (ID integer primary key, T text);";
-                    cmd.ExecuteNonQuery();
-
-                    //parameterized insert - more flexibility on parameter creation
-                    cmd.CommandText = @"INSERT INTO T1 (ID,T) VALUES(@id,@t)";
-
-                    cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
-                    {
-                        ParameterName = "@id",
-                        Value = 1
-                    });
-
-                    cmd.Parameters.Add(new System.Data.SQLite.SQLiteParameter
-                    {
-                        ParameterName = "@t",
-                        Value = "test2"
-                    });
-
-                    cmd.ExecuteNonQuery();
-
+                   
                     //read from the table
                     cmd.CommandText = @"SELECT ID, T FROM T1";
                     using (System.Data.SQLite.SQLiteDataReader reader = cmd.ExecuteReader())
